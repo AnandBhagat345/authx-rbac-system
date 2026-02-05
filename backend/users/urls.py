db2ca6a9b7dd12e60e212b20ed53651357446966
AnandBhagat345 <anandbhagat345@.com>
2026-02-05 19:52:00 +0530

    Add user registration and profile APIs with URL routing

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import UserProfileView
+from .views import UserProfileView, RegisterAPIView
 
 urlpatterns = [
-    path('me/', UserProfileView.as_view(), name='user-profile'),
+    # Auth related
+    path('auth/register/', RegisterAPIView.as_view(), name='register'),
+
+    # User related
+    path('users/me/', UserProfileView.as_view(), name='user-profile'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, status
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer,  RegisterSerializer
 
 # Create your views here.
 
@@ -14,4 +15,22 @@ class UserProfileView(APIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+    
+class RegisterAPIView(generics.CreateAPIView):
+    serializer_class  = RegisterSerializer
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response(
+            {
+                "message": "User registered successfully"
+            },
+            status=status.HTTP_201_CREATED
+        )
+
+    
+    
 
