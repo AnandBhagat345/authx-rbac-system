2fdc11e533fb7297c391e831c27627222c388792
AnandBhagat345 <anandbhagat345@.com>
2026-05-11 13:05:41 +0530

    now, project is ready to deply

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api/",
 });
 
 api.interceptors.request.use((config) => {
@@ -23,11 +23,17 @@ api.interceptors.response.use(
     ) {
       originalRequest._retry = true;
 
-      const refresh = localStorage.getItem("refresh");
+      
+      const refresh = localStorage.getItem("refresh")
+      if (!refresh) {
+         localStorage.clear() 
+         window.location.href = "/login" 
+         return Promise.reject(error)
+         }
 
       try {
         const res = await axios.post(
-          "http://127.0.0.1:8000/api/auth/token/refresh/",
+          import.meta.env.VITE_API_URL + "auth/token/refresh/",
           { refresh }
         );
 
